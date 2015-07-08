2f2dc4684c3f6898041b0bbcbb882146e4f8133e
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-07-07 21:21:12 -0400

    Working on more chapters and an LR improvement.

--- src/main/scala/chapitre-3/list.scala
+++ src/main/scala/chapitre-3/list.scala
@@ -17,4 +17,34 @@ object List {
   }
 
   def apply[A](as: A*): List[A] = if (as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](ls : List[A]) : List[A] =   drop(ls, 1)
+
+  def setHead[A](n: A, ls: List[A]): List[A] = ls match {
+    case Nil => Cons(n, Nil)
+    case Cons(h, t) => Cons(n, t)
+  }
+
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Nil => Nil
+    case _ if n == 0 => l
+    case Cons(h, t) if n > 0 => drop(t, n -1)
+  }
+
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+  }
+
 }
