4349667bb2c9fcd6bec24fe2198db629f0daba3d
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-08-08 21:35:32 -0400

    Added size operation and tests to tree.

--- src/test/scala/chapitre-3/TreeSpec.scala
+++ src/test/scala/chapitre-3/TreeSpec.scala
@@ -20,4 +20,41 @@ class TreeSpec extends FlatSpec with Matchers {
       case Branch(Branch(Leaf(left), Leaf(middle)), Leaf(right)) => (left, middle, right) shouldBe (10, 9, 8)
     }
   }
+
+  "Branch constructor" should "allow for null leaves" in {
+    val minitree = Branch(null, null)
+
+    minitree match {
+      case Branch(left, right) => (left, right) shouldBe (null, null)
+    }
+  }
+
+  "size" should "return 0 for a null tree" in {
+    Tree.size(null) shouldBe (0)
+  }
+
+  "size" should "return 1 for a tree with null branches" in {
+    Tree.size(Branch(null, null)) shouldBe (1)
+  }
+
+  "size" should "return 2 for a tree with one left leaf" in {
+    Tree.size(Branch(Leaf("e"), null)) shouldBe (2)
+  }
+
+  "size" should "return 2 for a tree with one right leaf" in {
+    Tree.size(Branch(null, Leaf(9))) shouldBe (2)
+  }
+
+  "size" should "return 4 for a tree with left only branches and a leaf 4 deep" in {
+    Tree.size(Branch(Branch(Branch(Leaf(5.4), null), null), null)) shouldBe (4)
+  }
+
+  "size" should "return 4 for a tree with right only branches and a leaf 4 deep" in {
+    Tree.size(Branch(null, Branch(null, Branch(null, Leaf(5.4))))) shouldBe (4)
+  }
+
+  "size" should "return 15 for a balanced tree 4 levels deep" in {
+    Tree.size(Branch(Branch(Branch(Leaf(5.4), Leaf(100.4)), Branch(Leaf(-5.4), Leaf(-100.4))), Branch(Branch(Leaf(665.4), Leaf(1009.4)), Branch(Leaf(-50.4), Leaf(10.4))))) shouldBe (15)
+  }
+
 }
