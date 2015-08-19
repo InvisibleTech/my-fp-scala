a7e2901faa0f857d1d892e774013ab2862d06fa2
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-08-18 22:44:04 -0400

    Revised some of my answers to Option functions to match better versions of answer code.

--- src/main/scala/chapitre-3/Option.scala
+++ src/main/scala/chapitre-3/Option.scala
@@ -19,6 +19,14 @@ sealed trait Option[+A] {
   def flatMap[B](f: A => Option[B]) : Option[B] = map(f) getOrElse None
 
   def orElse[B >: A](ob: => Option[B]) : Option[B] = map(Some(_)) getOrElse ob
+
+  def mean(xs: Seq[Double]) : Option[Double] = {
+    if (!xs.isEmpty) Some(xs.sum/xs.size) else None
+  }
+
+  def variance(xs: Seq[Double]) : Option[Double] ={
+    mean(xs) flatMap (m => mean(xs.map((x:Double) => math.pow(x - m , 2))))
+  }
 }
 
 case class Some[+A](get: A) extends Option[A]
